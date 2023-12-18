3417ffbd193b005af85f2f30efcd10603374d790
Facundo Leonel Cancian <facucancian@yahoo.com.ar>
2023-12-18 12:07:01 -0300

    homeMenu center

--- frontend/src/app/home/HomeMenu.tsx
+++ frontend/src/app/home/HomeMenu.tsx
@@ -1,15 +1,15 @@
-import HomeCard from "@/components/homeCard";
+import HomeCard from "@/components/HomeCard";
 import Box from "@mui/material/Box";
 import  Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
-const NAMES = ["Facturacion", "Productos", "Clientes", "Valores del Dolar"];
+const NAMES = ["Facturacion", "Productos", "Clientes"];
 
 export default function HomeMenu()
 {
     return (
         <>
-            <Grid item xs = {12}>
+            <Grid item xs = {12} textAlign={"center"}>
             <Typography>Control de Stock</Typography>
             </Grid>
             <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
@@ -19,6 +19,10 @@ export default function HomeMenu()
                     </Grid>)                
                 ) }
             </Grid>
+                <Box margin ={"auto"} width={"40%"} height={{xs:"2rem",sm:"2rem"}} textAlign={"center"} border={"1px solid black"} marginTop={2}>
+                <Typography>Valor del Dolar</Typography>
+                </Box>  
+
         </>
     );
 }
--- frontend/src/components/HomeCard.tsx
+++ frontend/src/components/HomeCard.tsx
@@ -6,7 +6,7 @@ interface Props {
   }
 export default function HomeCard(props: Props){
     return(
-        <Box width={"40%"} height={{xs:"2rem",sm:"2rem"}} textAlign={"center"} border={"1px solid black"}>
+        <Box margin={"auto"} width={"40%"} height={{xs:"2rem",sm:"2rem"}} textAlign={"center"} border={"1px solid black"} justifyContent="center"alignItems="center">
         <Typography>{props.title}</Typography>
         </Box>
     )
