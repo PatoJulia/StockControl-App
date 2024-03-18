584d901c0dfd71499a862ab844073d817550cc9b
Patricio Juliá <patricio.julia1@gmail.com>
2024-03-18 18:13:46 -0300

    More fixes for props typing

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <AuthComponent Component={children} />
+          <AuthComponent component={children} />
         </Providers>
       </body>
     </html>
